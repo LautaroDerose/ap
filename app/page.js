c7a21d9847fcf9facd57ea0a2d6f7d99ca16a908
Lautaro <lautiderose@gmail.com>
2024-03-11 01:11:21 -0300

    first

--- app/page.js
+++ app/page.js
@@ -1,113 +1,160 @@
-import Image from "next/image";
+import Image from 'next/image';
+import logo from "../public/logo.png"
+import { labelsHome, mock } from "@/lib/constants";
+import { Roboto, Work_Sans } from 'next/font/google';
+import backgroundImage from '../public/jeep-context-001.png'
+import Link from 'next/link';
+// import videoHome from "../public/banker.mp4"
+
+const work = Work_Sans({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export default function Home() {
+  
+  const labelClass = "px-4 py-2 border-white border-2 rounded-full hover:bg-white hover:text-black font-light  duration-300 "
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center ">
+      {/* <div className="h-[50vh] flex flex-col items-center justify-center relative">
+        <h1 className='text-8xl font-semibold z-10'>APRES.<span className='text-8xl font-extralight'>productora</span></h1>
+        <video autoPlay loop muted className="w-full h-2/3 object-cover filter grayscale hover:filter-none transition duration-300">          <source src="/banker.mp4" type="video/mp4" />
+        </video>
+      </div> */}
+      {/* <div className="h-[50vh] flex flex-col items-center justify-center relative">
+        <h1 className='text-8xl font-semibold z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+          APRES.<span className='text-8xl font-extralight'>productora</span>
+        </h1>
+        <video autoPlay loop muted className="w-full h-2/3 object-cover filter grayscale hover:filter-none transition duration-300">
+          <source src="/banker.mp4" type="video/mp4" />
+          Tu navegador no soporta la reproducción de videos.
+        </video>
+      </div> */}
+
+      <div className="h-[50vh] w-full flex flex-col items-center justify-center relative">
+        <div className='text-8xl font-semibold z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+          <h1>APRES.<span className='text-8xl font-extralight'>productora</span></h1>
+          <div className='flex flex-col items-center mt-8 '>
+            <div className='flex gap-4 items-center justify-center mb-2'>
+              <h2 className='text-sm border-r-2 p-2'>Eventos y experiencias</h2>
+              <h2 className='text-sm'>Producciones 360</h2>
+              <h2 className='text-sm border-l-2 p-2'>Activaciones BTL</h2>
+            </div>
+              <h2 className='text-sm flex items-center'>Based in Patagonia  <Link href={"/"} className='ml-2 '> |  Contact us!</Link></h2>
+          </div>
         </div>
+        <video autoPlay loop muted className="w-full h-full object-cover filter grayscale hover:filter-none transition duration-300">
+          <source src="/banker.mp4" type="video/mp4" />
+        </video>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* <div className="w-[800px] flex flex-wrap mx-auto gap-4 items-center py-8">
+        <div className="w-full text-center px-4 py-2 rounded-full border-2 hover:bg-white hover:text-black font-  duration-300 ">
+          <h2 className=''>{labelsHome[0].label}</h2>
+        </div>
+        <div className='flex w-full gap-4 '>
+          <div className={`${labelClass} w-full text-center `}>
+            <h2>{labelsHome[1].label}</h2>
+          </div>
+          <div className={`${labelClass} w-full text-center `}>
+            <h2>{labelsHome[2].label}</h2>
+          </div>
+          <div className={`${labelClass} w-full text-center `}>
+            <h2>{labelsHome[3].label}</h2>
+          </div>
+          
+        </div>
+        <div className='flex w-full gap-4'>
+          <div className={`${labelClass} w-full text-center `}>
+            <h2>{labelsHome[6].label}</h2>
+          </div>
+          <div className={`${labelClass} w-full text-center `}>
+            <h2>{labelsHome[7].label}</h2>
+          </div>
+          <div className={`${labelClass} w-full text-center `}>
+            <h2>{labelsHome[4].label}</h2>
+          </div>
+          <div className={`${labelClass} w-full text-center `}>
+            <h2>{labelsHome[5].label}</h2>
+          </div>
+        </div>
+        <div className="w-full bg-white text-center text-black font-bold px-4 py-2 rounded-full border-2 hover:bg-black hover:text-white duration-300 hover:text-lg  ">
+          <h2>{labelsHome[8].label}</h2>
+        </div>
+      </div> */}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="w-screen flex h-auto border-2 border-white">
+          <div className='w-4/12 border-r-2 border-b-2 relative group'>
+            <Image src={backgroundImage} layout="fill" objectFit="cover" className="z-0" />
+            <div className='absolute inset-0 bg-black opacity-80 transition duration-300 ease-in-out z-10 group-hover:opacity-0'></div>
+            <div className='flex h-full items-center justify-center relative z-20'>
+              <h1 className='text-8xl font-bold group-hover:text-black duration-300'>{mock[0].label}</h1>
+            </div>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div className='w-8/12 py-6 px-10 border-b-2'>
+            <h1 className={`w-fit mx-auto text-6xl text-center border-b-4 mt-4 mb-4 ${work.className}`}>EXPERIENCIA JEEP GLADIATOR</h1>
+            <p className='pt-4'>En medio de la majestuosidad natural de la ruta de los 7 lagos, nos aventuramos hacia nuestro destino final en el <a className='uppercase border-b-2 '>@lagohermososki</a>, donde la naturaleza se despliega en su máximo esplendor. Cada curva nos presenta una nueva sorpresa, con vistas impresionantes que cautivan nuestros sentidos y nos invitan a explorar más allá de lo conocido. Es un viaje donde la aventura se entrelaza con la belleza, creando recuerdos que perdurarán en nuestro corazón.</p>
+            <h2>Feat: <a>@btlandia</a></h2>
+          </div>
+        </div>
+      
+      <div className="w-screen flex h-auto ">
+        <div className='flex h-fit '>
+          <div className='w-8/12 py-6 px-10 border-b-2'>
+            <h1 className={`w-fit mx-auto text-6xl text-center border-b-4 mt-4 mb-4 uppercase ${work.className}`}>Lanzamiento de Producto</h1>
+            <p className='pt-4'>Celebramos el lanzamiento de  <a className='uppercase border-b-2 '>@bosquegin</a> en Villa La Angostura con un vibrante after lake. La música y los DJs en vivo acompañaron la degustación de este gin tan patagónico y sus aromáticas notas. Sumergidos en la atmósfera de los lagos, los invitados exploraron los sabores únicos mientras se deleitaban con la majestuosidad natural que rodea cada momento de esta experiencia inolvidable.</p>
+            {/* <h2>Feat: <a>@btlandia</a></h2> */}
+          </div>
+          <div className='w-4/12   border-l-2  border-b-2'>
+            <div className='flex h-full items-center justify-center'>
+              <h1 className='text-8xl font-bold'>{mock[1].label}</h1>
+            </div>
+          </div>
+        </div>
+        
       </div>
     </main>
   );
 }
+
+// import Image from "next/image";
+// import logo from "../public/logo.png"
+// import { labelsHome } from "@/lib/constants";
+
+
+// export default function Home() {
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-end   ">
+//       <div>
+//         <Image src={logo} width={300} height={200} style={logoStyle} />
+//       </div>
+//       <div className="w-[700px] flex flex-wrap mx-auto gap-4 p-8 items-center " >
+//         {
+//           labelsHome.map((label) => (
+//             <div className=" px-4 py-2 border-white border-2 rounded-full ">
+//               <h2>
+//                 {label.label}
+//               </h2>
+//             </div>
+//           ))
+//         }
+//       </div>
+//       <div className="w-[800px] h-[60vh] border-2 border-white ">
+//       </div>
+//     </main>
+//   );
+// }
+
+// Que hacemos?
+// Eventos
+// Brand 
+// Experience
+// Activaciones BTL
+// Branding
+// Produccion
+// Planning
+// PR
+// Contactanos!
